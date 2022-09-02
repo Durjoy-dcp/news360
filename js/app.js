2c8cf78791f27b71ce0a87b3806d63d9aa66976e
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-03 02:24:57 +0600

    catagories added

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// console.log("Its working ");
+
+const fetchCatagory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => displayCatagories(data.data.news_category))
+        .catch((error) => console.log(error));
+}
+function displayCatagories(data) {
+    console.log(data);
+    const listParent = document.getElementById("catagory");
+    data.forEach(element => {
+        const li = document.createElement('li');
+
+        // li.classList.add = "mx-4";
+
+        li.innerHTML = `<a class="nav-link" id="${element.category_name}" aria-current="page" href="#">${element.category_name}</a>`;
+        listParent.appendChild(li);
+    });
+}
+
+fetchCatagory();
